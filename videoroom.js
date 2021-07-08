3640032c42701c513501efe24ce4dca63ee5cfea
emsbn <emsbn1@gmail.com>
2021-07-08 14:47:57 +0900

    fix: videoroom

--- videoroom.js
+++ videoroom.js
@@ -43,10 +43,10 @@
 // the whole session.
 //
 var server = null;
-// if (window.location.protocol === "http:")
-//   server = "http://" + window.location.hostname + ":8088/janus";
-// else server = "https://" + window.location.hostname + ":8089/janus";
-server = "wss://" + window.location.hostname + ":8989/ws";
+if (window.location.protocol === "http:")
+  server = "http://" + window.location.hostname + ":8088/janus";
+else server = "https://" + window.location.hostname + ":8089/janus";
+server = "wss://" + window.location.hostname + ":8989";
 
 var janus = null;
 var sfutest = null;
