8a222b4623177e421a28297c4f7ec9ef4eddaa19
emsbn <emsbn1@gmail.com>
2021-07-08 14:29:36 +0900

    fix: videoroom id

--- videoroomtest.js
+++ videoroomtest.js
@@ -518,7 +518,7 @@ function registerUsername() {
     }
     var register = {
       request: "join",
-      room: parseInt($("#room_id").val(), 10) || myroom,
+      room: myroom,
       ptype: "publisher",
       display: username,
     };
